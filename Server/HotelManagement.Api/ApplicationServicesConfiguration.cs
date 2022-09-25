eeabebb01b8d0b2f32514292d5f8ca5da62a97ea
Dawid Bańczak <dawid.banczak@mipcloud.pl>
2022-09-25 13:59:06 +0200

    Configured automapper

---
+++ Server/HotelManagement.Api/ApplicationServicesConfiguration.cs
@@ -0,0 +1,14 @@
+﻿using HotelManagement.Api.Profiles;
+
+namespace HotelManagement.Api
+{
+    public static class ApplicationServicesConfiguration
+    {
+        public static IServiceCollection RegisterApplicationServices(this IServiceCollection services)
+        {
+            services.AddAutoMapper(typeof(MappingProfile));
+
+            return services;
+        }
+    }
+}
