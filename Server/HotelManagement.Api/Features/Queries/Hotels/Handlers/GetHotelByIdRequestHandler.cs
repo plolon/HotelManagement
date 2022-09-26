da04ed9724f4e6dad9abb4e40d7b0a801b4ad51a
Dawid Bańczak <dawid.banczak@mipcloud.pl>
2022-09-26 10:52:37 +0200

    Added get by id hotels endpoint

---
+++ Server/HotelManagement.Api/Features/Queries/Hotels/Handlers/GetHotelByIdRequestHandler.cs
@@ -0,0 +1,25 @@
+﻿using AutoMapper;
+using HotelManagement.Api.DTOs.Hotel;
+using HotelManagement.Api.Features.Queries.Hotels.Requests;
+using HotelManagement.Domain.Repositories;
+using MediatR;
+
+namespace HotelManagement.Api.Features.Queries.Hotels.Handlers
+{
+    public class GetHotelByIdRequestHandler : IRequestHandler<GetHotelByIdRequest, HotelDto>
+    {
+        private readonly IHotelRepository _hotelRepository;
+        private readonly IMapper _mapper;
+
+        public GetHotelByIdRequestHandler(IHotelRepository hotelRepository, IMapper mapper)
+        {
+            _hotelRepository = hotelRepository;
+            _mapper = mapper;
+        }
+        public async Task<HotelDto> Handle(GetHotelByIdRequest request, CancellationToken cancellationToken)
+        {
+            var hotel = await _hotelRepository.Get(request.Id);
+            return _mapper.Map<HotelDto>(hotel);
+        }
+    }
+}
---
+++ Server/HotelManagement.Api/Features/Queries/Hotels/Requests/GetHotelByIdRequest.cs
@@ -0,0 +1,10 @@
+﻿using HotelManagement.Api.DTOs.Hotel;
+using MediatR;
+
+namespace HotelManagement.Api.Features.Queries.Hotels.Requests
+{
+    public class GetHotelByIdRequest : IRequest<HotelDto>
+    {
+        public int Id { get; set; }
+    }
+}
