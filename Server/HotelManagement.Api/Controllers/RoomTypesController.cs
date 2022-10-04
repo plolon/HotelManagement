7e29e4d322a70bc59013d854b2da80ffa0afa861
Dawid Bańczak <dawid.banczak@mipcloud.pl>
2022-10-04 13:43:42 +0200

    Created roomtype controller and dto

---
+++ Server/HotelManagement.Api/Controllers/RoomTypesController.cs
@@ -0,0 +1,31 @@
+﻿using HotelManagement.Api.DTOs.RoomType;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HotelManagement.Api.Controllers;
+
+[Route("api/[controler]")]
+[ApiController]
+public class RoomTypesController : ControllerBase
+{
+    private readonly IMediator _mediator;
+
+    public RoomTypesController(IMediator mediator)
+    {
+        _mediator = mediator;
+    }
+
+    // GET: api/<RoomTypesController>
+    [HttpGet]
+    public async Task<ICollection<RoomTypeDto>> Get()
+    {
+        throw new NotImplementedException();
+    }
+    
+    // GET: api/<RoomTypesController>
+    [HttpGet("{id}")]
+    public async Task<RoomTypeDto> Get(int id)
+    {
+        throw new NotImplementedException();
+    }
+}
---
+++ Server/HotelManagement.Api/DTOs/RoomType/RoomTypeDto.cs
@@ -0,0 +1,8 @@
+﻿namespace HotelManagement.Api.DTOs.RoomType;
+
+public class RoomTypeDto
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    
+}
