e466f58cbd9153442341d627afd0bad77e6d4039
Jakub <jakub.barczewski@netguru.com>
2022-10-05 21:38:04 +0200

    Add Extension to ValidatorResult for error Generation

---
+++ Server/HotelManagement.Api/Extensions/ValidatorExtensions.cs
@@ -0,0 +1,20 @@
+using FluentValidation;
+using FluentValidation.Results;
+
+namespace HotelManagement.Api.Extensions
+{
+    public static class ValidatorExtensions
+    {
+        public static string GenerateErrorMessages(
+            this ValidationResult validator
+        )
+        {
+            var message = "";
+            validator.Errors
+                .Select(x => x.ErrorMessage)
+                .ToList()
+                .ForEach(x => message += x);
+            return message;
+        }
+    }
+}
--- Server/HotelManagement.Api/Features/Commands/Hotels/Handlers/CreateHotelRequestHandler.cs
+++ Server/HotelManagement.Api/Features/Commands/Hotels/Handlers/CreateHotelRequestHandler.cs
@@ -1,5 +1,7 @@
 ﻿using AutoMapper;
+using FluentValidation.Results;
 using HotelManagement.Api.DTOs.Hotel;
+using HotelManagement.Api.Extensions;
 using HotelManagement.Api.Features.Commands.Hotels.Requests;
 using HotelManagement.Domain.Models;
 using HotelManagement.Domain.Repositories;
@@ -8,31 +10,34 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace HotelManagement.Api.Features.Commands.Hotels.Handlers
 {
-    public class CreateHotelRequestHandler : IRequestHandler<CreateHotelRequest, HotelDto>
+    public class
+        CreateHotelRequestHandler : IRequestHandler<CreateHotelRequest,
+            HotelDto>
     {
         private readonly IHotelRepository _hotelRepository;
         private readonly IMapper _mapper;
-        public CreateHotelRequestHandler(IHotelRepository hotelRepository, IMapper mapper)
+
+        public CreateHotelRequestHandler(IHotelRepository hotelRepository,
+            IMapper mapper)
         {
             _hotelRepository = hotelRepository;
             _mapper = mapper;
         }
-        public async Task<HotelDto> Handle(CreateHotelRequest request, CancellationToken cancellationToken)
+
+        public async Task<HotelDto> Handle(CreateHotelRequest request,
+            CancellationToken cancellationToken)
         {
             var hotelDtoValidator = new HotelDtoValidator();
-            var validationResult = await hotelDtoValidator.ValidateAsync(request.SaveHotelDto);
+            var validationResult =
+                await hotelDtoValidator.ValidateAsync(request.SaveHotelDto);
 
             if (!validationResult.IsValid)
             {
-                var message = "";
-                validationResult.Errors
-                    .Select(x => x.ErrorMessage)
-                    .ToList()
-                    .ForEach(x => message += x);
+                var message = validationResult.GenerateErrorMessages();
                 //TODO: Implement custom Exception class for HttpErrors
                 throw new Exception(message);
             }
-            
+
             var hotel = _mapper.Map<Hotel>(request.SaveHotelDto);
             hotel = await _hotelRepository.Add(hotel);
             return _mapper.Map<HotelDto>(hotel);
