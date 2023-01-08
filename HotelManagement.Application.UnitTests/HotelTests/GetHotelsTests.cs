4f2aad9b424476169ccc93ec928742d913835dc6
Jakub <jakub.barczewski@netguru.com>
2023-01-09 00:13:19 +0100

    Correct invoked method, add tests for GetHotelById

--- HotelManagement.Application.UnitTests/HotelTests/GetHotelsTests.cs
+++ HotelManagement.Application.UnitTests/HotelTests/GetHotelsTests.cs
@@ -4,7 +4,6 @@ using HotelManagement.Application.DTOs.Hotel;
 using HotelManagement.Application.Features.Hotels.Queries.Handlers;
 using HotelManagement.Application.Features.Hotels.Queries.Requests;
 using HotelManagement.Application.Profiles;
-using HotelManagement.Application.UnitTests.Mocs.Repositories;
 using HotelManagement.Domain.Models;
 using HotelManagement.Domain.Repositories;
 using Moq;
@@ -87,6 +86,105 @@ namespace HotelManagement.Application.UnitTests.HotelTests
                 CancellationToken.None);
 
             res.Should().NotBeEmpty().Equals(hotelDtos);
+        }
+
+        [Fact]
+        public async Task Handle_GetById_WithValidId_ReturnsHotel()
+        {
+            var hotels = new List<Hotel>
+            {
+                new Hotel
+                {
+                    City = "Warschau",
+                    Country = "Polen",
+                    Id = 1,
+                    Name = "Jugend",
+                    Street = "Mustache",
+                    DateCreated = new DateTime(1939, 09, 01),
+                    DateModified = new DateTime(1945, 09, 02)
+                },
+                new Hotel
+                {
+                    City = "Wollstein",
+                    Country = "Polen",
+                    Id = 2,
+                    Name = "Volks",
+                    Street = "Wagen",
+                    DateCreated = new DateTime(1937, 05, 28),
+                    DateModified = new DateTime(1945, 09, 02)
+                }
+            };
+
+            var hotelDtos = new List<HotelDto>
+            {
+                new HotelDto
+                {
+                    DateCreated = new DateTime(1939, 09, 01),
+                    DateModified = new DateTime(1945, 09, 02),
+                    Id = 1,
+                    Name = "Jugend",
+                    Address = new HotelAddressDto
+                    {
+                        City = "Warschau",
+                        Country = "Polen",
+                        Street = "Mustache",
+                    }
+                },
+                new HotelDto
+                {
+                    DateCreated = new DateTime(1937, 05, 28),
+                    DateModified = new DateTime(1945, 09, 02),
+                    Id = 2,
+                    Name = "Volks",
+                    Address = new HotelAddressDto
+                    {
+                        City = "Wollstein",
+                        Country = "Polen",
+                        Street = "Wagen",
+                    }
+                },
+            };
+
+            var unitOfWork = new Mock<IUnitOfWork>();
+            unitOfWork.Setup(x => x.Hotels.Get(It.IsAny<int>()))
+                .Returns(async (int id) => hotels[id]);
+
+            var mapperConfig = new MapperConfiguration(x =>
+            {
+                x.AddProfile<MappingProfile>();
+            });
+            var mapper = mapperConfig.CreateMapper();
+
+            var handler =
+                new GetHotelByIdRequestHandler(unitOfWork.Object, mapper);
+
+            var res = await handler.Handle(new GetHotelByIdRequest { Id = 1 },
+                CancellationToken.None);
+
+            res.Should().NotBeNull().Equals(hotelDtos[0]);
+        }
+
+        [Fact]
+        public async Task Handle_GetById_WithNotExistingId_ReturnsNull()
+        {
+            var unitOfWork = new Mock<IUnitOfWork>();
+            unitOfWork.Setup(x => x.Hotels.Get(It.IsAny<int>()))
+                .Returns(async () => null);
+
+            var mapperConfig = new MapperConfiguration(x =>
+            {
+                x.AddProfile<MappingProfile>();
+            });
+
+            var mapper = mapperConfig.CreateMapper();
+
+            var handler =
+                new GetHotelByIdRequestHandler(unitOfWork.Object, mapper);
+
+            var res = await handler.Handle(new GetHotelByIdRequest { Id = 1 },
+                CancellationToken.None);
+
+            res.Should().BeNull();
         }
     }
 }
--- HotelManagement.Application/Features/Hotels/Queries/Handlers/GetHotelByIdRequestHandler.cs
+++ HotelManagement.Application/Features/Hotels/Queries/Handlers/GetHotelByIdRequestHandler.cs
@@ -6,19 +6,24 @@ using HotelManagement.Domain.Repositories;
 
 namespace HotelManagement.Application.Features.Hotels.Queries.Handlers
 {
-    public class GetHotelByIdRequestHandler : IQueryHandler<GetHotelByIdRequest, HotelDto>
+    public class
+        GetHotelByIdRequestHandler : IQueryHandler<GetHotelByIdRequest,
+            HotelDto>
     {
         private readonly IUnitOfWork _unitOfWork;
         private readonly IMapper _mapper;
 
-        public GetHotelByIdRequestHandler(IUnitOfWork unitOfWork, IMapper mapper)
+        public GetHotelByIdRequestHandler(IUnitOfWork unitOfWork,
+            IMapper mapper)
         {
             _unitOfWork = unitOfWork;
             _mapper = mapper;
         }
-        public async Task<HotelDto> Handle(GetHotelByIdRequest request, CancellationToken cancellationToken)
+
+        public async Task<HotelDto> Handle(GetHotelByIdRequest request,
+            CancellationToken cancellationToken)
         {
-            var hotel =  await _unitOfWork.Hotels.GetAll();
+            var hotel = await _unitOfWork.Hotels.Get(request.Id);
             return _mapper.Map<HotelDto>(hotel);
         }
     }
