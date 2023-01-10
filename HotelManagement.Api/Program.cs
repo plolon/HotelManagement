725a8112fbe632939dc5b222a1646e095bc4268c
Jakub <jakub.barczewski@netguru.com>
2023-01-10 01:02:25 +0100

    Temp run of prepDb & Swagger

--- HotelManagement.Api/Program.cs
+++ HotelManagement.Api/Program.cs
@@ -27,13 +27,13 @@ var app = builder.Build();
 
 app.UseAuthentication();
 
-if (app.Environment.IsDevelopment()) // always IsDevelopment
-{
+//if (app.Environment.IsDevelopment()) // always IsDevelopment
+//{
     app.PrepareDatabase();
     app.UseSwagger();
     app.UseSwaggerUI(c =>
         c.SwaggerEndpoint("/swagger/v1/swagger.json", "HotelManagement.Api v1"));
-}
+//}
 
 app.UseSerilogRequestLogging();
 
